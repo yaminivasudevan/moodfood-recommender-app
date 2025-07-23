c00bc4b5e62c832ff07f1203d3a48f4449154781
Yamini K <yaminivasudevan96@gmail.com>
2025-07-23 11:51:59 +0400

    ResourceNotFoundException added

---
+++ src/main/java/com/yamini/moodfood/exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.yamini.moodfood.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException() {
+        super();
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
